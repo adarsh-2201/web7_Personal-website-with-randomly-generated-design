5859eafe7dabe231e168290e62b1b7360ae43ff1
Dev Daftari <thequad2017@gmail.com>
2021-06-25 15:34:25 +0530

    Added icon and footer
    Made some changes in index.html

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
-const num= Math.floor(Math.random()*4);
+const num= Math.floor(Math.random()*5);
 
-document.getElementById('css_sheet').setAttribute('href',`${num}.css`);
+// document.getElementById('css_sheet').setAttribute('href',`${num}.css`);
+// const num=4
+
+function GetCssSheet(num){
+    if(num===4){
+        const name="Dev Daftari";
+        document.getElementById("logo").innerHTML="DEV";
+        document.getElementsByTagName("title")[0].innerHTML=`${name} - Personal Website`;
+        document.getElementById('css_sheet').setAttribute('href',`${num}.css`);
+        document.getElementById("footer").innerHTML=`Copyright &copy ${name} - All rights reserved.`;
+    }
+    else{
+        document.getElementById('css_sheet').setAttribute('href',`${num}.css`);
+    }
+    
+}
+GetCssSheet(num);
